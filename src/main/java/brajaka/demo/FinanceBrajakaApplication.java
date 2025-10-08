57d53fe5f3ae89f396083825d2e9822657804899
Forando Hotman <andositopu@gmail..com>
2025-10-08 17:53:18 +0700

    hapus redis

--- src/main/java/brajaka/demo/FinanceBrajakaApplication.java
+++ src/main/java/brajaka/demo/FinanceBrajakaApplication.java
@@ -2,12 +2,11 @@ package brajaka.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableCaching
+//@EnableCaching
 public class FinanceBrajakaApplication {
 
 	public static void main(String[] args) {
